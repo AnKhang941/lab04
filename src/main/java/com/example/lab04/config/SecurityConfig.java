076c10b328bb87f265a82a5c64a6ffcee8c4c143
AnKhang941 <ankhangnguyen941@gmail.com>
2026-03-15 10:01:26 +0700

    lab06

--- src/main/java/com/example/lab04/config/SecurityConfig.java
+++ src/main/java/com/example/lab04/config/SecurityConfig.java
@@ -5,11 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -24,15 +23,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(accountService);
-        provider.setPasswordEncoder(passwordEncoder());
-        return provider;
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -42,13 +33,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authenticationProvider(authenticationProvider())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/products/add", "/products/edit/**", "/products/delete/**").hasRole("ADMIN")
-                        .requestMatchers("/products/**").hasAnyRole("USER", "ADMIN")
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/products").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/products/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .formLogin(withDefaults());
+                .formLogin(withDefaults())
+                .logout(withDefaults());
         return http.build();
     }
 }
